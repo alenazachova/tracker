dc2a7d69d0703100f7758314cfc7cd93b3149cc9
alenazachova <ala.zachova@gmail.com>
2024-01-11 00:52:31 +0100

    adding env file

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,13 @@ import "./App.css";
 import { Login } from "./components/Auth/Login";
 import { Home } from "./components/Home/Home";
 import { ErrorPage } from "./components/Error/ErrorPage";
-import { Layout } from "./components/Layout/Layout";
 import { Timer } from "./components/Timer/Timer";
-import { Logo } from "./components/Logo";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const supabase = createClient(
-  "https://dyqzeefgdzzumaskxjlr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5cXplZWZnZHp6dW1hc2t4amxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MzA3MzQsImV4cCI6MjAyMDQwNjczNH0.SyF6cMN290oqB9EJdyd3F7hnH9Lk3JQy8BVqFYyzoHI"
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
 function App() {
@@ -22,7 +20,7 @@ function App() {
   const isLoggedIn = token ? true : false;
 
   return (
-    <GoogleOAuthProvider clientId="872976465576-1p8kl9lavhsqj276ostn9clpqglkgh3h.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home loggedIn={isLoggedIn} />} />
