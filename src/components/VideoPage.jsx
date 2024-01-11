c934866d10bbf59a914249e71231012b9fc6e29a
alenazachova <ala.zachova@gmail.com>
2024-01-11 14:58:37 +0100

    add video

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -1,3 +1,10 @@
 export const VideoPage = () => {
-  return <video src={"./video.mp4"} />;
+  // In public folder
+  return (
+    <video
+      controls={true}
+      src={"./video.mp4"}
+      style={{ display: "block", width: "100%", height: "100%" }}
+    />
+  );
 };
