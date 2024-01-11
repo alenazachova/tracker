c12b7d06c70a75e9323344508901ad9ad9b06821
alenazachova <64907088+alenazachova@users.noreply.github.com>
2024-01-11 15:11:19 +0100

    video-page-fix (#12)
    * video-page-fix
    
    * video-page-fix-link
    
    * link fix
    
    * link fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <Box component={"footer"} sx={{ padding: 1, marginTop: "auto" }}>
+    <Box
+      component={"footer"}
+      sx={{ padding: 1, marginTop: "auto", display: "flex" }}
+    >
       <Typography variant="body1">
         © {new Date().getFullYear()} TimeTracker. All rights reserved.
       </Typography>
+      <Link to={"/video"}>video</Link>
     </Box>
   );
 };
